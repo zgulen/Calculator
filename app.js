56dc563838fd5280b3e1d30f14b3bbe10469e72f
zgulen <gulenzubeyir@gmail.com>
2022-06-13 14:12:22 +0300

    Calculator

---
+++ app.js
@@ -0,0 +1,74 @@
+const prevDisp = document.querySelector(".previous-display")
+const currDisp = document.querySelector(".current-display")
+
+const btnContainer = document.querySelector(".buttons-container")
+let currOperand = ""
+let prevOperand = ""
+let operation = ""
+
+btnContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("num")) {
+    appendNumber(e.target.textContent)
+    updateDisplay()
+  }
+  if (e.target.classList.contains("operator")) {
+    chooseOperator(e.target.textContent)
+    updateDisplay()
+  }
+  if (e.target.classList.contains("equal")) {
+    calculate()
+    updateDisplay()
+  }
+})
+
+
+const appendNumber = (num) => {
+  if (num !== "." && currOperand === "0"){
+    currOperand = num
+    return
+  } 
+    if (num === "." && currOperand.includes(".")) return
+    if (currOperand.length > 9) return
+    currOperand += num
+}
+
+const updateDisplay = () => {
+  currDisp.textContent = currOperand
+  prevDisp.textContent = `${prevOperand} ${operation}`
+
+}
+
+const chooseOperator = (op) =>{
+  if (prevOperand){
+    calculate()
+  }
+  operation = op
+  prevOperand = currOperand
+  currOperand = ""
+}
+
+const calculate = () =>{
+  let calculation = 0;
+  const prev = Number(prevOperand)
+  const curr = Number(currOperand)
+  switch (operation) {
+    case "+":
+      calculation = prev + curr
+      break;
+    case "-":
+      calculation = prev - curr
+      break;
+    case "x":
+      calculation = prev * curr
+      break;
+    case "÷":
+      calculation = prev / curr
+      break;
+  
+    default:
+      return;
+  }
+  currOperand = calculation
+  prevOperand = ""
+  operation = ""
+}
